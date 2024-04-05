b151664035320cbd8e7d9c7e64583ba6759b7ec3
yksl <yukselkurtuldu9@gmail.com>
2024-04-05 14:21:13 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import mailSvg from "./assets/mail.svg";
+import manSvg from "./assets/man.svg";
+import womanSvg from "./assets/woman.svg";
+import manAgeSvg from "./assets/growing-up-man.svg";
+import womanAgeSvg from "./assets/growing-up-woman.svg";
+import mapSvg from "./assets/map.svg";
+import phoneSvg from "./assets/phone.svg";
+import padlockSvg from "./assets/padlock.svg";
+
+const url = "https://randomuser.me/api/";
+const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="block bcg-orange">
+      </div>
+      <div className="block">
+        <div className="container">
+          <img src={defaultImage} alt="random user" className="user-img" />
+          <p className="user-title">My ... is</p>
+          <p className="user-value"></p>
+          <div className="values-list">
+            <button className="icon" data-label="name">
+              <img src={womanSvg} alt="user" id="iconImg" />
+            </button>
+            <button className="icon" data-label="email">
+              <img src={mailSvg} alt="mail" id="iconImg" />
+            </button>
+            <button className="icon" data-label="age">
+              <img src={womanAgeSvg} alt="age" id="iconImg" />
+            </button>
+            <button className="icon" data-label="street">
+              <img src={mapSvg} alt="map" id="iconImg" />
+            </button>
+            <button className="icon" data-label="phone">
+              <img src={phoneSvg} alt="phone" id="iconImg" />
+            </button>
+            <button className="icon" data-label="password">
+              <img src={padlockSvg} alt="lock" id="iconImg" />
+            </button>
+          </div>
+          <div className="btn-group">
+            <button className="btn" type="button">
+              new user
+            </button>
+            <button className="btn" type="button">
+              add user
+            </button>
+          </div>
+
+          <table className="table">
+            <thead>
+              <tr className="head-tr">
+                <th className="th">Firstname</th>
+                <th className="th">Email</th>
+                <th className="th">Phone</th>
+                <th className="th">Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="body-tr"></tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+      </div>
+    </main>
   );
 }
 
